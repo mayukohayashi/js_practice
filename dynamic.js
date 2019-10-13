8ea8011bcfc5d736f9e5635554ad222ce63f0242
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-13 21:23:39 +0900

    fun lesson

---
+++ dynamic.js
@@ -0,0 +1,8 @@
+// JavaScript is  Dynamic language
+
+// typeof name
+// "string"
+// name = 1;
+// 1
+// typeof name
+// "number"
---
+++ variable.js
@@ -0,0 +1,12 @@
+// Can not be a reserved keyword, let, if like this.
+// Should be meaningful. variable name should be lovable.
+// Can not start with a number
+// Can not contain a space or hyphen (-)
+// Are case-sensitive
+
+
+let name = 'moosh'; // string literal
+let age = 30; // number literal
+let isApproved = true; // boolean literal. true or false
+let firstName = undefiened;
+let lastName = null;
