6fc9aec05a3a2c38f89e71dcfdd202b4f8f4df27
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-15 01:42:21 +0900

    some work

---
+++ functions.js
@@ -0,0 +1,18 @@
+
+// performing a task
+
+function greet(name, lastName) {
+  console.log('Hello' + name + ' ' + lastName);
+}
+
+// calculating a value
+function square(number) {
+  return number * number;
+}
+
+
+
+greet('John', 'Smith');
+
+let number = square(2);
+//  console.log(square(2));
---
+++ logical.js
@@ -0,0 +1,11 @@
+//logical and (&&)
+//returns TRUE if both operands are TRUE
+
+let highIncome = true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome && goodCreditScore;
+
+//
+let applicationRefund = !eligibleForLoan
+
+console.log(eligibleForLoan);
