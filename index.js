0d7ad1f014aa06690aaebd8285560bed24b320ae
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-15 22:40:58 +0900

    lessons

---
+++ index.js
@@ -0,0 +1,88 @@
+/*****************************
+* Variables and data types
+*/
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and MArk';
+var if = 23;
+*/
+
+
+
+/*****************************
+* Variable mutation and type coercion
+*/
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+
+
+/*****************************
+* Basic operators
+*/
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yeahMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older tha John');
+var x;
+console.log(typeof x);
+*/
+
