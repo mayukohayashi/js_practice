78aef66828c220efe4f8e436757bba7048e26971
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-16 20:36:04 +0900

    Update script.js

--- howJSWorksBehindtheScenes/script.js
+++ howJSWorksBehindtheScenes/script.js
@@ -1,6 +1,7 @@
 /////////////////////////////////////
 // Lecture: Hoisting
 
+
 // functions
 calculateAge(1965);
 
@@ -8,25 +9,25 @@ function calculateAge(year) {
     console.log(2016 - year);
 }
 
- 
-// // retirement(1956);
-// var retirement = function(year) {
-//     console.log(65 - (2016 - year));
-// }
+
+// retirement(1956);
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
 
-// // variables
+// variables
 
-// console.log(age);
-// var age = 23;
+console.log(age);
+var age = 23;
 
-// function foo() {
-//     console.log(age);
-//     var age = 65;
-//     console.log(age);
-// }
-// foo();
-// console.log(age);
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
