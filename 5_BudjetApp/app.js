d45d98d39c426dd605ef1925bf5d93303cfcce7f
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-18 20:42:52 +0900

    Update app.js

--- 5_BudjetApp/app.js
+++ 5_BudjetApp/app.js
@@ -21,7 +21,28 @@ var budgetController = (function() {
 // UI controller
 var UIController = (function(){
 
-  //some code
+  var DOMStrings = {
+    inputType:        '.add__type',
+    inputDescription: '.add__description',
+    inputValue:       '.add__value',
+    inputBtn:        '.add__btn'
+  }
+  return {
+    getInput: function() {
+
+      return {
+        type:        document.querySelector(DOMStrings.inputType).value, // will be either "inc" or "exp"
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value:       document.querySelector(DOMStrings.inputValue).value
+      };
+
+    },
+
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+
+  };
 
 })();
 
@@ -37,10 +58,15 @@ var UIController = (function(){
 // Global App controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMStrings();
+
   var ctrlAddItem = function() {
       // console.log('OK');
 
       // 1. Get the field input data
+      var input = UICtrl.getInput();
+      console.log(input);
+
 
       // 2. Add the item to the budget controller
 
@@ -51,14 +77,14 @@ var controller = (function(budgetCtrl, UICtrl) {
       // 5. Display the budget on the UI
 
   }
-
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
 
       if(event.keyCode === 13 || event.which === 13){
         ctrlAddItem();
       }
+
 
 
     });
