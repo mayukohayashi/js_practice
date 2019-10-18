6268c04b702c6bc8d985f2d535f94f0ac2605a0d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-18 13:11:39 +0900

    in progress

--- 4_ObjectFunctionAdvanced/script.js
+++ 4_ObjectFunctionAdvanced/script.js
@@ -32,3 +32,24 @@ console.log(jane.lastName);
 console.log(mark.lastName);
 */
 
+
+/////////////////////////////
+// Lecture: Object.create
+/*
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
+*/
