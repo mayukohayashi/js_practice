8c62cd7f51057bef1ecb6a2c19b2af7b4dd66557
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-20 17:17:19 +0900

    excersise

--- 6_ES6ES2015/script.js
+++ 6_ES6ES2015/script.js
@@ -93,6 +93,7 @@ console.log(c);
 /////////////////////////////////
 // Lecture: Strings
 
+/*
 let firstName = 'john';
 let lastName = 'Smith';
 const yearOfBirth = 1990;
@@ -624,21 +625,102 @@ johnAthlete6.calculateAge();
 
 /*
 
-Suppose that you're working in a small town administration, and you're in charge of two town elements:
+Suppose that you're working in a small town administration,
+and you're in charge of two town elements:
 1. Parks
 2. Streets
 
-It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+It's a very small town, so right now there are only 3 parks and 4 streets
+. All parks and streets have a name and a build year.
 
 At an end-of-year meeting, your boss wants a final report with the following:
 1. Tree density of each park in the town (formula: number of trees/park area)
 2. Average age of each town's park (formula: sum of all ages/number of parks)
 3. The name of the park that has more than 1000 trees
 4. Total and average length of the town's streets
-5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+5. Size classification of all streets: tiny/small/normal/big/huge.
+If the size is unknown, the default is normal
 
 All the report data should be printed to the console.
 
 HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
 
 */
+
+
+class CityHold {
+  constructor (name, yearOfMade) {
+    this.name = name;
+    this.yearOfMade = yearOfMade;
+  }
+}
+
+
+
+class Parks extends CityHold {
+  constructor(name, yearOfMade, sizeOfPark, numOfTrees) {
+
+    super(name, yearOfMade);
+    this.sizeOfPark = sizeOfPark;
+    this.numOfTrees = numOfTrees;
+  }
+
+  //calculate density of park up to number of trees
+  calucDensity() {
+    const densityOfTrees = this.numOfTrees / this.sizeOfPark
+    console.log(`${this.name} has a tree density of ${densityOfTrees} square km.`);
+  }
+}
+
+
+
+
+
+class Streets extends CityHold {
+  constructor(name, yearOfMade, streetLength, size = 3) {
+    super(name, yearOfMade);
+    this.streetLength = streetLength;
+    this.size = size;
+  }
+
+    //length classification
+    classifyStreet() {
+      const classification = new Map();
+      classification.set(1, 'tiny');
+      classification.set(2, 'small');
+      classification.set(3, 'normal');
+      classification.set(4, 'big');
+      classification.set(5, 'huge');
+      console.log(`${this.name}, build in ${this.yearOfMade}, is a ${classification.get(this.size)}`)
+    }
+}
+
+
+const allParks = [new Park('Green Park', 1987, 0.2, 215),
+                  new Park('National Park', 1894, 2.9, 3541),
+                  new Park('Oak Park', 1953, 0.4, 949)];
+
+const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4),
+                    new Street('Evergreen Street', 2008, 2.7, 2),
+                    new Street('4th Street', 2015, 0.8),
+                    new Street('Sunset Boulevard', 1982, 2.5, 5)];
+
+
+function reportPark(p) {
+  console.log('---------------- PARKS REPORT------------------')
+
+  //Density
+
+  //Average age
+
+  //Which park has more than 100%
+  
+
+}
+
+function reportStreet(s) {
+
+}
+
+reportPark(allParks);
+reportStreet(allStreets);
