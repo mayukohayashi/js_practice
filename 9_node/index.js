29141699ba0d725d744c5d4804954699d70bf869
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-21 23:33:14 +0900

    in progress

--- 9_node/index.js
+++ 9_node/index.js
@@ -47,10 +47,16 @@ const server = http.createServer((req, res) => {
 
   const pathName =  req.url;
 
-  if(pathName === '/overview') {
+  if(pathName === '/' || pathName === '/overview') {
     res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
   }
 
 
