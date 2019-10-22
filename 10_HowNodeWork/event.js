fca94d476e0933e7f563304a41e598453765354d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-22 19:39:49 +0900

    learning streaming

--- 10_HowNodeWork/event.js
+++ 10_HowNodeWork/event.js
@@ -1,12 +1,14 @@
 const EventEmitter = require('events');
+const http = require('http')
 
-const myEmitter = new EventEmitter();
 
 class Sales extends EventEmitter {
   constructor() {
     super();
   }
 }
+
+const myEmitter = new EventEmitter();
 
 myEmitter.on('newSale', () => {
   console.log('there was a new sale!')
@@ -21,3 +23,32 @@ myEmitter.on('newSale', stock => {
 })
 
 myEmitter.emit('newSale', 9);
+
+
+
+
+///////////////////////////
+
+
+
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received')
+  console.log(req.url);
+  res.end('Request received');
+})
+
+
+server.on('request', (req, res) => {
+  console.log('another request');
+})
+
+server.on('click', () => {
+  console.log('Server closed')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Waiting for requests....')
+})
