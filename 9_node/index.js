8436069c0292f59af9d6eb7612c74c300e44a0d9
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-22 12:46:58 +0900

    overview page

--- 9_node/index.js
+++ 9_node/index.js
@@ -68,13 +68,12 @@ const dataObj      = JSON.parse(data);
   // res.end(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  // console.log(req.url);
 
-  const pathName =  req.url;
-
+const { query, pathname } = url.parse(req.url, true);
 
   // OVERVIEW PAGE
-  if(pathName === '/' || pathName === '/overview') {
+  if(pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {'Content-type': 'text/html'});
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -85,11 +84,16 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
 // product page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT')
+  } else if (pathname === '/product') {
+    // console.log(query);
+    res.writeHead(200, {'Content-type': 'text/html'});
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output)
 
 // api page
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
       res.writeHead(200, {'Content-type': 'application/json'});
       res.end(data);
 
