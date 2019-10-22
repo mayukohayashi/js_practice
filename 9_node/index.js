edb2f4e31fed83e315357e8bf1190f91ecdf26c1
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-22 12:10:51 +0900

    make overviewpage
    finally can get kinda. res.end and so on

--- 9_node/index.js
+++ 9_node/index.js
@@ -41,10 +41,27 @@ console.log('Will read file');
 
 ////////////////////////////////////////////////
 //////SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+      output = output.replace(/{%IMAGE%}/g, product.image);
+      output = output.replace(/{%PRICE%}/g, product.price);
+      output = output.replace(/{%FROM%}/g, product.from);
+      output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+      output = output.replace(/{%QUANTITY%}/g, product.quantity);
+      output = output.replace(/{%DESCRIPTION%}/g, product.description);
+      output = output.replace(/{%ID%}/g, product.id);
 
+      if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+      return output;
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data);
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct  = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard     = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
+const data         = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj      = JSON.parse(data);
   // console.log(productData);
 
   // res.writeHead(200, { 'Content-type': 'application/json' });
@@ -58,9 +75,14 @@ const server = http.createServer((req, res) => {
 
   // OVERVIEW PAGE
   if(pathName === '/' || pathName === '/overview') {
-    
+    res.writeHead(200, {'Content-type': 'text/html'});
 
-    res.end('This is the OVERVIEW');
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    // console.log(cardsHtml);
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+
+    res.end(output);
 
 // product page
   } else if (pathName === '/product') {
