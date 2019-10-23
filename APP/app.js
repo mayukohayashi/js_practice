9547973d9faba532064e42c79abd0833afb2a50f
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-23 15:54:32 +0900

    middleware

--- APP/app.js
+++ APP/app.js
@@ -1,8 +1,19 @@
 const fs = require('fs')
 const express = require('express');
-const app = express();
 
+const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware');
+  next();
+})
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -10,9 +21,12 @@ const tours = JSON.parse(
 
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
     results: tours.length,
+    requestedAt: req.requestTime,
     data: {
       tours
     }
