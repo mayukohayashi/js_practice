8c84279191e7b37c65d339bd3eb5e7190c607436
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-23 18:38:49 +0900

    bugged

--- APP/app.js
+++ APP/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -14,12 +14,12 @@ app.use(express.json());
 app.use((req, res, next) => {
   console.log('Hello from middleware');
   next();
-})
+});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 
 
