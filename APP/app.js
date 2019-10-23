919a47a27288377e6861a56b1486c522630c74ee
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-23 14:04:36 +0900

    routing

--- APP/app.js
+++ APP/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -27,6 +31,42 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+app.get('/api/v1/tours/:id/', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id)
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+})
+
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({id: newId}, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
