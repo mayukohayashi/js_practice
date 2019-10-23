413c36ccdd60fa00b6386629a9ba8d3ff08f85ca
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-23 17:26:38 +0900

    create server.js

--- APP/app.js
+++ APP/app.js
@@ -29,7 +29,5 @@ app.use('/api/v1/users', userRouter);
 
 
 // 4. start server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}.....`);
-});
+
+module.exports = app;
