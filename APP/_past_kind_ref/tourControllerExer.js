391f0f63155946a60a71b47c199c2f0b11b8589b
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-25 01:53:18 +0900

    create tour model

--- APP/_past_kind_ref/tourControllerExer.js
+++ APP/_past_kind_ref/tourControllerExer.js
@@ -1,4 +1,8 @@
-const Tour = require('./../models/tourModel');
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
 
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is: ${val}`);
@@ -28,10 +32,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours
-    // }
+    results: tours.length,
+    data: {
+      tours
+    }
   });
 };
 
@@ -39,24 +43,35 @@ exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
 
-  // const tour = tours.find(el => el.id === id);
+  const tour = tours.find(el => el.id === id);
 
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour
-  //   }
-  // });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
 };
 
 exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success'
-    // data: {
-    //   tour: newTour
-    // }
-  });
-}
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+};
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
--- APP/server.js
+++ APP/server.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
 
+dotenv.config({
+  path: './config.env'
+});
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
