b0718f49ded1e3536718c0a0e33660ead47dc5d6
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-25 17:07:10 +0900

    refactoring controller

--- APP/controllers/tourController.js
+++ APP/controllers/tourController.js
@@ -40,14 +40,26 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success'
-    // data: {
-    //   tour: newTour
-    // }
-  });
-}
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new tour({})
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
