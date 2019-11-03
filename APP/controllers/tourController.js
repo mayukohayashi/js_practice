e5ae5773ee7c8c5f63c879937c79ec13e8adb85d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-03 20:22:40 +0900

    add delete method

--- APP/controllers/tourController.js
+++ APP/controllers/tourController.js
@@ -81,7 +81,7 @@ exports.updateTour = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour: '<Updated tour here...>'
+        tour
       }
     });
   } catch (err) {
@@ -92,9 +92,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
