dbf7c227e28093f7dfd5dac23b98c00792e092c3
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-12 13:47:00 +0900

    feat: top5tours alias

--- APP/controllers/tourController.js
+++ APP/controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is: ${val}`);
 
