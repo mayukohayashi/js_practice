aac8827646da6a43d0393802564699788756df50
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-15 13:41:26 +0900

    add: matching and grouping

--- APP/routes/tourRoutes.js
+++ APP/routes/tourRoutes.js
@@ -10,6 +10,10 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
+  .route('/tour-stats')
+  .get(tourController.getTourStats);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
