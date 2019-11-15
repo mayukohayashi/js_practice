839382118eb945a9945866ac3d7f2807100fdac4
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-15 16:38:57 +0900

    feat: aggregation middleware

--- APP/models/tourModel.js
+++ APP/models/tourModel.js
@@ -94,14 +94,20 @@ tourSchema.pre('save', function(next) {
 // tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
-
   this.start = Date.now();
   next();
 });
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!!`)
-  console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 
