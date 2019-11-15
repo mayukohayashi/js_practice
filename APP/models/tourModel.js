7d90928be830741874731f4dcc181d1682667c64
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-15 16:21:24 +0900

    feat; query middleware

--- APP/models/tourModel.js
+++ APP/models/tourModel.js
@@ -54,7 +54,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: {
@@ -85,6 +89,21 @@ tourSchema.pre('save', function(next) {
 //   console.log(doc);
 //   next();
 // })
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!!`)
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
