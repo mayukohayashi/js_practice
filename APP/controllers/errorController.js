b2ee371644d9c7571e810c0296ec537a89f7455d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-17 13:28:42 +0900

    feat: handling dupulicate db field

--- APP/controllers/errorController.js
+++ APP/controllers/errorController.js
@@ -3,6 +3,12 @@ const AppError = require('./../utils/appError')
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`
   return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please user another value!`;
+  return new AppError(message, 400);
 }
 
 const sendErrorDev = (err, res) => {
@@ -45,6 +51,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
 
     sendErrorProd(error, res);
