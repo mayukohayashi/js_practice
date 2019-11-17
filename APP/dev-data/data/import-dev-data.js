998b45638c85e5ddefbb5ae1240cee43137225fa
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-17 14:13:58 +0900

    feat: handling errors outside of express

--- APP/dev-data/data/import-dev-data.js
+++ APP/dev-data/data/import-dev-data.js
@@ -14,6 +14,7 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
   })
   .then(() => console.log('DB connection successful!'));
--- APP/server.js
+++ APP/server.js
@@ -16,11 +16,20 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
   })
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION! SHUTTING down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
