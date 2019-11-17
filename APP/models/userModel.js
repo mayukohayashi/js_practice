71081de6d085b3183625aaa0570fc4e46a39e980
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-17 16:44:57 +0900

    feat: encrypt user password with bicypt

--- APP/models/userModel.js
+++ APP/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-// name, email. photo, password, passswordConfirm
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -22,8 +22,27 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please provide a password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //This only works on CREATE and SAVE!
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords and confirmation passwords are not the same!'
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passweordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
