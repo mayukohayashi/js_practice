d92030871df2abd08ebfdafc41ebfbe79dcb40e5
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-27 16:39:24 +0900

    try: authorize function for login with cookies

--- APP/app.js
+++ APP/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -45,6 +46,7 @@ app.use(
     limit: '10kb'
   })
 );
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -69,7 +71,7 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 
