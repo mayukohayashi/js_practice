e244de91dd9d23c424c6db0937ebf4cfccfbaa24
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-28 18:34:50 +0900

    add: npm install multer

--- APP/routes/userRoutes.js
+++ APP/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer')
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
