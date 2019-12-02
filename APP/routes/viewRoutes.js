1c70a37157100c14c1324b8f68f550ca6169f996
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-02 13:05:59 +0900

    add: rendering booking page

--- APP/routes/viewRoutes.js
+++ APP/routes/viewRoutes.js
@@ -15,8 +15,9 @@ router.get(
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 // /login
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
-
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
+
 router.post(
   '/submit-user-data',
   authController.protect,
